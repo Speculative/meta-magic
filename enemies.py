2339c869feb2c3393ce3030779225db7a5397143
Jeff Tao <jatao88@gmail.com>
2019-03-06 18:41:00 -0800

    Enemies can move diagonally

--- enemies.py
+++ enemies.py
@@ -3,13 +3,13 @@ import level
 
 def approach_player(game_state, own_state):
     terrain_map = game_state["level"]
-    distances = level.distance_map(terrain_map, game_state["player_pos"])
+    distances = level.diagonal_distance_map(terrain_map, game_state["player_pos"])
     own_id, own_type, own_health, own_pos = own_state
 
     # find the step that is closest to the player
     candidate_steps = [
         (n, level.level_get(distances, n))
-        for n in level.neighbors(terrain_map, own_pos)
+        for n in level.neighbors(terrain_map, own_pos, level.ALL_DIRECTIONS)
     ]
     best_step, best_step_distance = sorted(candidate_steps, key=lambda c: c[1])[0]
 
@@ -24,4 +24,3 @@ def approach_player(game_state, own_state):
 ENEMY_TYPES = {
     "zombie": {"init_health": 10, "behaviors": [approach_player], "icon": "z"}
 }
-
--- level.py
+++ level.py
@@ -1,6 +1,16 @@
 import math
 
-CARDINAL = {"N": (-1, 0), "S": (1, 0), "E": (0, 1), "W": (0, -1)}
+CARDINAL_DIRECTIONS = {"N": (-1, 0), "S": (1, 0), "E": (0, 1), "W": (0, -1)}
+ALL_DIRECTIONS = {
+    "N": (-1, 0),
+    "S": (1, 0),
+    "E": (0, 1),
+    "W": (0, -1),
+    "NE": (-1, 1),
+    "NW": (-1, -1),
+    "SE": (1, 1),
+    "SW": (1, -1),
+}
 
 
 def make_room(rows, cols):
@@ -37,35 +47,43 @@ def in_bounds(level, loc):
     return 0 <= row < rows and 0 <= col < cols
 
 
-def neighbor_values(level, loc):
-    return list(map(lambda n: level_get(level, n), neighbors(level, loc)))
+def neighbor_values(level, loc, directions):
+    return list(map(lambda n: level_get(level, n), neighbors(level, loc, directions)))
 
 
-def neighbors(level, loc):
+def neighbors(level, loc, directions):
     return list(
         filter(
             lambda n: in_bounds(level, n),
-            [add_coord(loc, dir) for dir in CARDINAL.values()],
+            [add_coord(loc, dir) for dir in directions.values()],
         )
     )
 
 
-def distance_map(level, goal):
+def cardinal_distance_map(level, goal):
+    return distance_map(level, goal, CARDINAL_DIRECTIONS)
+
+
+def diagonal_distance_map(level, goal):
+    return distance_map(level, goal, ALL_DIRECTIONS)
+
+
+def distance_map(level, goal, directions):
     goal_row, goal_col = goal
     rows, cols = dimensions(level)
     weights = [[math.inf for col in range(cols)] for row in range(rows)]
     weights[goal_row][goal_col] = 0
-    frontier = neighbors(level, goal)
+    frontier = neighbors(level, goal, directions)
     while len(frontier) > 0:
         node = frontier.pop(0)
         node_weight = level_get(weights, node)
         node_terrain = level_get(level, node)
         # skip wall tiles
         if not node_terrain:
-            lowest = sorted(neighbor_values(weights, node))[0]
+            lowest = sorted(neighbor_values(weights, node, directions))[0]
             if node_weight > lowest + 1:
                 level_set(weights, node, lowest + 1)
-                frontier.extend(neighbors(level, node))
+                frontier.extend(neighbors(level, node, directions))
 
     return weights
 
@@ -86,4 +104,3 @@ def print_weights(weights):
     )
     for line in weights:
         print("".join(map(str, line)))
-
